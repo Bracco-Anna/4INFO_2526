19551db933fd19713c7d2451f2c0f61ac089ad63
Bracco-Anna <anna.bracco@istitutoagnelli.it>
2025-11-06 15:59:35 +0100

    es 09

---
+++ TPSI/bracco_anna/javascript/bracco_esercizio_js_09.js
@@ -0,0 +1,7 @@
+function login(){
+    let user = prompt("what's your name?") || alert("canceled");
+    user == null || user  == 'Admin' || alert("i don't know you");
+    user == 'Admin' && (password = prompt("password"));
+    user == null || password == null || password == "Agnellino" || alert("wrong password");
+    password == "Agnellino" && alert("welcome");
+}
